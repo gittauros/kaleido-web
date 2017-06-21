0324f44f0782f18e0320994e897cf206fb06ddf9
gittauros <zhy12081992@126.com>
2017-06-21 23:23:02 +0800

    关闭入口

--- src/main/java/com/tauros/kaleido/web/controller/HomeController.java
+++ src/main/java/com/tauros/kaleido/web/controller/HomeController.java
@@ -16,8 +16,8 @@ public class HomeController extends BaseController {
 
     @RequestMapping({"/", "home", "index"})
     public String homePage(HttpServletRequest request, HttpServletResponse response, Model model) {
-        model.addAttribute("request", JSON.toJSONString(request));
-        model.addAttribute("response", JSON.toJSONString(response));
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
         model.addAttribute("message", "Welcome to Kaleido!");
         model.addAttribute("message_cn", "欢迎来到万花镜!");
         return "index";
