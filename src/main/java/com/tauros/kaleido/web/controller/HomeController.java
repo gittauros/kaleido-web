3b7d69a2cffa0b870492c3f2feb43de05169e09a
gittauros <zhy12081992@126.com>
2017-06-21 23:04:59 +0800

    fastjson & gson

--- src/main/java/com/tauros/kaleido/web/controller/HomeController.java
+++ src/main/java/com/tauros/kaleido/web/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.tauros.kaleido.web.controller;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,13 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class HomeController extends BaseController {
 
-	@RequestMapping({"/", "home", "index"})
-	public String homePage(HttpServletRequest request, HttpServletResponse response, Model model) {
-		model.addAttribute("request", request);
-		model.addAttribute("response", response);
-		model.addAttribute("message", "Welcome to Kaleido!");
-		model.addAttribute("message_cn", "欢迎来到万花镜!");
-		return "index";
-	}
+    @RequestMapping({"/", "home", "index"})
+    public String homePage(HttpServletRequest request, HttpServletResponse response, Model model) {
+        model.addAttribute("request", JSON.toJSONString(request));
+        model.addAttribute("response", JSON.toJSONString(response));
+        model.addAttribute("message", "Welcome to Kaleido!");
+        model.addAttribute("message_cn", "欢迎来到万花镜!");
+        return "index";
+    }
 
 }
